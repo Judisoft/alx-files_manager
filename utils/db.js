d2db215abb57e513e074b6ab165be14eb876fd9b
Judisoft <kumjude09@gmail.com>
2023-09-05 18:06:59 +0100

    create mongo db client

---
+++ utils/db.js
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb');
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.PORT || 27017;
+const dbName = process.env.DB_DATABASE || 'file_manager';
+
+class DBClient {
+  constructor() {
+    this.db = null;
+    MongoClient.connect(
+      `mongodb://${host}:${port}/${dbName}`,
+      { useUnifiedTopology: true },
+      (err, client) => {
+        if (err) console.log(err);
+        this.db = client.db(dbName);
+        this.db.createCollection('users');
+        this.db.createCollection('files');
+      },
+    );
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() { return this.db.collection('users').countDocuments(); }
+
+  async nbFiles() { return this.db.collection('files').countDocuments(); }
+
+  async findUser(user) { return this.db.collection('users').findOne(user); }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
